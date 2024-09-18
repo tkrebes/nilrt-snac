3a6de1e43ed3c522977a20276d0974b873031381
Mark Silva <mark.silva@ni.com>
2024-09-18 14:45:49 -0500

    Install libpwquality when configuring pwquality
    Signed-off-by: Mark Silva <mark.silva@ni.com>

--- nilrt_snac/_configs/_pwquality_config.py
+++ nilrt_snac/_configs/_pwquality_config.py
@@ -5,16 +5,18 @@ from nilrt_snac._configs._base_config import _BaseConfig
 from nilrt_snac._configs._config_file import _ConfigFile
 
 from nilrt_snac import logger
+from nilrt_snac.opkg import opkg_helper
 
 
 class _PWQualityConfig(_BaseConfig):
     def __init__(self):
-        pass
+        self._opkg_helper = opkg_helper
 
     def configure(self, args: argparse.Namespace) -> None:
         print("Configuring Password quality...")
         config_file = _ConfigFile("/etc/pam.d/common-password")
         dry_run: bool = args.dry_run
+        self._opkg_helper.install("libpwquality")
 
         if not config_file.contains("remember=5"):
             config_file.update(r"(password.*pam_unix.so.*)", r"\1 remember=5")
@@ -34,6 +36,9 @@ class _PWQualityConfig(_BaseConfig):
         print("Verifying Password quality...")
         config_file = _ConfigFile("/etc/pam.d/common-password")
         valid = True
+        if not self._opkg_helper.is_installed("libpwquality"):
+            valid = False
+            logger.error("MISSING: libpwquality not installed")
         if not config_file.contains("remember=5"):
             valid = False
             logger.error("MISSING: 'remember=5' for pam_unix.so configuration")
