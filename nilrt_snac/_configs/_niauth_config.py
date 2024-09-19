ad1fd6d53c2c5ab4cb5dc97ea2808ca015464884
Alex Hearn <alex.hearn@ni.com>
2024-09-19 12:27:26 -0500

    nilrt-snac: remove niacctbase-sudo package
    The niacctbase-sudo package installs a sudo configuration file that
    gives special privileges to any account named admin.
    
    Signed-off-by: Alex Hearn <alex.hearn@ni.com>

--- nilrt_snac/_configs/_niauth_config.py
+++ nilrt_snac/_configs/_niauth_config.py
@@ -15,6 +15,7 @@ class _NIAuthConfig(_BaseConfig):
         print("Removing NIAuth...")
         dry_run: bool = args.dry_run
         self._opkg_helper.remove("ni-auth", force_essential=True, force_depends=True)
+        self._opkg_helper.remove("niacctbase-sudo")
         if not self._opkg_helper.is_installed("nilrt-snac-conflicts"):
             self._opkg_helper.install(str(SNAC_DATA_DIR / "nilrt-snac-conflicts.ipk"))
 
@@ -28,6 +29,9 @@ class _NIAuthConfig(_BaseConfig):
         if self._opkg_helper.is_installed("ni-auth"):
             valid = False
             logger.error("FOUND: ni-auth installed")
+        if self._opkg_helper.is_installed("niacctbase-sudo"):
+            valid = False
+            logger.error("FOUND: niacctbase-sudo installed")
         if not self._opkg_helper.is_installed("nilrt-snac-conflicts"):
             valid = False
             logger.error("MISSING: nilrt-snac-conflicts not installed")
