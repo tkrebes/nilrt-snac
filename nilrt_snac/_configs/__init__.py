2b91668621892a837e47e63f423b109618c8c20f
Alex Hearn <alex.hearn@ni.com>
2024-11-12 10:51:43 -0600

    nilrt-snac: install ni-sysapi-sshcli
    System API requires this package in order to communicate from a
    host to a target over ssh.
    
    Signed-off-by: Alex Hearn <alex.hearn@ni.com>

--- nilrt_snac/_configs/__init__.py
+++ nilrt_snac/_configs/__init__.py
@@ -11,6 +11,7 @@ from nilrt_snac._configs._opkg_config import _OPKGConfig
 from nilrt_snac._configs._pwquality_config import _PWQualityConfig
 from nilrt_snac._configs._ssh_config import _SshConfig
 from nilrt_snac._configs._sudo_config import _SudoConfig
+from nilrt_snac._configs._sysapi_config import _SysAPIConfig
 from nilrt_snac._configs._tmux_config import _TmuxConfig
 from nilrt_snac._configs._wifi_config import _WIFIConfig
 from nilrt_snac._configs._wireguard_config import _WireguardConfig
@@ -29,6 +30,7 @@ CONFIGS: List[_BaseConfig] = [
     _FaillockConfig(),
     _X11Config(),
     _ConsoleConfig(),
+    _SysAPIConfig(),
     _TmuxConfig(),
     _PWQualityConfig(),
     _SshConfig(),
---
+++ nilrt_snac/_configs/_sysapi_config.py
@@ -0,0 +1,23 @@
+import argparse
+
+from nilrt_snac._configs._base_config import _BaseConfig
+
+from nilrt_snac import logger
+from nilrt_snac.opkg import opkg_helper
+
+
+class _SysAPIConfig(_BaseConfig):
+    def __init__(self):
+        self._opkg_helper = opkg_helper
+
+    def configure(self, args: argparse.Namespace) -> None:
+        print("Configuring SysAPI...")
+        self._opkg_helper.install("ni-sysapi-sshcli")
+
+    def verify(self, args: argparse.Namespace) -> bool:
+        print("Verifying SysAPI configuration...")
+        valid = True
+        if not self._opkg_helper.is_installed("ni-sysapi-sshcli"):
+            valid = False
+            logger.error("MISSING: ni-sysapi-sshcli not installed")
+        return valid
