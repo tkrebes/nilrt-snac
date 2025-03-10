61450d89de54c3b94648db9d9516381331194648
Eli Engelhardt <eli.engelhardt@emerson.com>
2025-03-10 18:49:20 -0500

    Configure fixes ssh verify errors
    Signed-off-by: Eli Engelhardt <eli.engelhardt@emerson.com>

--- nilrt_snac/_configs/_ssh_config.py
+++ nilrt_snac/_configs/_ssh_config.py
@@ -8,10 +8,37 @@ from nilrt_snac import logger
 
 class _SshConfig(_BaseConfig):
     def __init__(self):
-        pass  # Nothing to do for now
+        self.ssh_config_path = "/etc/ssh/sshd_config"
+        self.tmout_config_path = "/etc/profile.d/tmout.sh"
+        self.client_alive_interval = "ClientAliveInterval 15"
+        self.client_alive_count_max = "ClientAliveCountMax 4"
+        self.tmout = "TMOUT=600"
 
     def configure(self, args: argparse.Namespace) -> None:
-        pass  # Nothing to do for now
+        sshd_config_file = _ConfigFile(self.ssh_config_path)
+        tmout_config_file = _ConfigFile(self.tmout_config_path)
+        dry_run: bool = args.dry_run
+
+        if not sshd_config_file.contains_exact(self.client_alive_interval):
+            if sshd_config_file.contains("ClientAliveInterval"):
+                sshd_config_file.update("ClientAliveInterval.*", self.client_alive_interval)
+            else:
+                sshd_config_file.add(self.client_alive_interval)
+        
+        if not sshd_config_file.contains_exact(self.client_alive_count_max):
+            if sshd_config_file.contains("ClientAliveCountMax"):
+                sshd_config_file.update("ClientAliveCountMax.*", self.client_alive_count_max)
+            else:
+                sshd_config_file.add(self.client_alive_count_max)
+        sshd_config_file.save(dry_run)
+
+        if not tmout_config_file.contains_exact(self.tmout):
+            if tmout_config_file.contains("TMOUT"):
+                tmout_config_file.update("TMOUT.*", self.tmout)
+            else:
+                tmout_config_file.add(self.tmout)
+        tmout_config_file.save(dry_run)
+
 
     def verify(self, args: argparse.Namespace) -> bool:
         print("Verifying ssh configuration...")
@@ -21,16 +48,16 @@ class _SshConfig(_BaseConfig):
         if not sshd_config_file.exists():
             valid = False
             logger.error(f"MISSING: {sshd_config_file.path} not found")
-        elif not sshd_config_file.contains_exact("ClientAliveInterval 15"):
+        elif not sshd_config_file.contains_exact(self.client_alive_interval):
             valid = False
             logger.error("MISSING: expected ClientAliveInterval value")
-        elif not sshd_config_file.contains_exact("ClientAliveCountMax 4"):
+        elif not sshd_config_file.contains_exact(self.client_alive_count_max):
             valid = False
             logger.error("MISSING: expected ClientAliveCountMax value")
         if not tmout_config_file.exists():
             valid = False
             logger.error(f"MISSING: {tmout_config_file.path} not found")
-        elif not tmout_config_file.contains_exact("TMOUT=600"):
+        elif not tmout_config_file.contains_exact(self.tmout):
             valid = False
             logger.error("MISSING: expected TMOUT value")
         return valid
